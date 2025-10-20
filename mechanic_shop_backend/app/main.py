1bb9955f38e278b891f011bd11a90032e617e9bd
Joshua D Smith <2smithjosh@gmail.com>
2025-10-20 15:17:08 -0700

    updated extensions route in main.py

--- mechanic_shop_backend/app/main.py
+++ mechanic_shop_backend/app/main.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from ..config import Config
-from app.extensions import db, migrate, limiter, cache
+from .extensions import db, migrate, limiter, cache
 
 app = Flask(__name__)
 app.config.from_object(Config)
