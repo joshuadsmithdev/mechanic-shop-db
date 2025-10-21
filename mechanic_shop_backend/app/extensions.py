1175739674c3e488726f0611ab44afe4e59aae2a
joshuadsmithdev <joshua.d.smith.dev@gmail.com>
2025-10-21 12:47:54 -0700

    Update extensions.py

--- mechanic_shop_backend/app/extensions.py
+++ mechanic_shop_backend/app/extensions.py
@@ -5,7 +5,11 @@ from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from flask_caching import Cache
 
-limiter = Limiter(key_func=get_remote_address)
+limiter = Limiter(
+  key_func=get_remote_address,
+  storage_uri="memory://",   # ✅ in-memory store (perfect for Render)
+    default_limits=[]          # ✅ optional; disables global default rate limits
+)
 cache = Cache(config={'CACHE_TYPE': 'SimpleCache'})
 
 db     = SQLAlchemy()
